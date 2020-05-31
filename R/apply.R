465dd64c6490d48909fbfe540bfe2493593978e9
kaneplusplus <kaneplusplus@gmail.com>
2020-05-31 13:01:02 -0400

    added apply simplify argument.

--- R/apply.R
+++ R/apply.R
@@ -22,7 +22,7 @@
 #' x[,] <- round(rnorm(10))
 #' biganalytics::apply(x, 1, mean)
 setMethod('apply', signature(X="big.matrix"),
-  function(X, MARGIN, FUN, ...) return(bmapply(X, MARGIN, FUN, ...)))
+  function(X, MARGIN, FUN, ..., simplify) return(bmapply(X, MARGIN, FUN, ...)))
 
 bmapply <- function(X, MARGIN, FUN, ...)
 {
